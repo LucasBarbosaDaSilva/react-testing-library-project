3b3ee6deda286237fb191a1c08058bf2c36ea41b
LucasBarbosaDaSilva <l.barbosatkd@gmail.com>
2023-01-23 20:20:55 -0300

    Req 2 passando nos testes

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -9,4 +9,10 @@ describe('Teste o componente About.js ', () => {
 
     screen.getByRole('heading', { level: 2, name: /About Pokédex/i });
   });
+  test('Teste se a página contém a seguinte imagem de uma Pokédex', async () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const saveImg = getByRole('img', { src: 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png' });
+    expect(saveImg).not.toBeNull();
+    expect(saveImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente NotFound.js', () => {
+  test('É exibido na tela um h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    screen.getByRole('heading', { level: 2, name: /Page requested not found/i });
+  });
+  test('Teste se a página contém a seguinte imagem de uma Pokédex', async () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const saveImg = getByRole('img', { src: 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif' });
+    expect(saveImg).not.toBeNull();
+    expect(saveImg).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
